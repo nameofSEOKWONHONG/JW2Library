92976ff06f85d1bde13e7b562ee91f72b2872b9c
SeokWon Hong <h20913@gmail.com>
2021-03-29 01:51:17 +0900

    add d2d project

---
+++ d2dlib-winform/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using unvell.D2DLib;
+using unvell.D2DLib.WinForm;
+
+namespace d2dlib_winform
+{
+    public partial class Form1 : D2DForm {
+        private D2DPoint _d2DPoint = new D2DPoint(0, 0);
+        private D2DSize _d2DSize = new D2DSize(10, 10);
+        
+        public Form1()
+        {
+            InitializeComponent();
+            base.AnimationDraw = true;
+            base.ShowFPS = true;
+        }
+
+        protected override void OnFrame() {
+            base.OnFrame();
+            
+            _d2DPoint.x += 1;
+            _d2DPoint.y += 1;
+        }
+
+        protected override void OnRender(D2DGraphics g) {
+            base.OnRender(g);
+            var rect = new D2DRect(_d2DPoint, _d2DSize);
+            g.DrawRectangle(rect, D2DColor.Red);
+        }
+    }
+}
---
+++ d2dlib-winform/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace d2dlib_winform
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
