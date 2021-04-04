ddcc4b035bd086b252e3e428b9342aadbf74e626
SeokWon Hong <h20913@gmail.com>
2021-04-04 21:34:24 +0900

    add jsubstring, jsplit of span implement

--- JWLibrary.Core/JString.cs
+++ JWLibrary.Core/JString.cs
@@ -1,9 +1,67 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
 using System.Text;
+using NetFabric.Hyperlinq;
 
 namespace JWLibrary.Core {
+    ref struct SplitSpanEnumerator {
+        private ReadOnlySpan<char> text;
+        private readonly char splitChar;
+        
+        public ReadOnlySpan<char> Current { get; private set; }
+        
+        public SplitSpanEnumerator(ReadOnlySpan<char> text, char splitChar) {
+            this.text = text;
+            this.splitChar = splitChar;
+            this.Current = default;
+        }
+
+        public SplitSpanEnumerator GetEnumerator() => this;
+
+        public bool MoveNext() {
+            var index = text.IndexOf(splitChar);
+            if (index == -1)
+                return false;
+
+            Current = text[..index];
+            text = text[(index + 1)..];
+
+            return true;
+        }
+    }
+    
     public static class JString {
+        /*
+         * Span변환의 장점 : 스택에 메모리 할당되므로 GC가 발생하지 않도록 해줌.
+         * Memory<T> (T:Class) -> Span<T> (T:Struct)로 변환하여 사용 가능
+         * new 대신 stackalloc 사용할 경우 GC 압력이 줄어듬.(struct type에 대하여, int, char, byte 등등)
+         * windows stack 최대 할당 용량은 1MB
+         */
+        public static string Test(this string str) {
+            Span<char> s = stackalloc char[100];
+            Span<int> ss = stackalloc int[100];
+            return string.Empty;
+        }
+            
+        public static string jSubstring(this string str, int startIndex, int length = 0) {
+            if (length > 0) {
+                return str.AsSpan()[startIndex..(startIndex + length)].ToString();
+            }
+
+            return str.AsSpan()[startIndex..str.Length].ToString();
+        }
+
+        public static IEnumerable<string> jSplit(this string str, char splitChar) {
+            JList<string> result = new JList<string>();
+            foreach (var @char in new SplitSpanEnumerator(str.AsSpan(), splitChar)) {
+                result.Add(@char.ToString());
+            }
+
+            return result;
+        }
+
         public static int jToCount(this string str) {
             return str.jIsNull() ? 0 : str.Length;
         }
--- JWLibrary.NUnit.Test/JStringTest.cs
+++ JWLibrary.NUnit.Test/JStringTest.cs
@@ -1,4 +1,5 @@
-﻿using JWLibrary.Core;
+﻿using System;
+using JWLibrary.Core;
 using NUnit.Framework;
 
 namespace JWLibrary.NUnit.Test {
@@ -23,6 +24,25 @@ namespace JWLibrary.NUnit.Test {
 
             index = "test".jLastIndexOf("");
             Assert.Less(index, 0);
+        }
+
+        [Test]
+        public void JSplitTest() {
+            var text = "1|2|3|4|5|6|7|8|9|10";
+            var result = text.jSplit('|');
+            result.jForEach(item => {
+                Console.WriteLine(item);
+            });
+            
+            Assert.Pass();
+        }
+
+        [Test]
+        public void JSubstringTest() {
+            var text = "세상이 바뀌어도 그 본질은 바뀌지 않는다.";
+            var result = text.jSubstring(4, 1);
+            Assert.AreEqual("바", result);
+            
         }
     }
 }
