788bc1c500587369d0cae41644e432b40bc760b3
SeokWon Hong <h20913@gmail.com>
2021-04-16 19:45:47 +0900

    임시커밋

--- JWLibrary/Database/JDatabase.cs
+++ JWLibrary/Database/JDatabase.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver.Core.Connections;
 using MySql.Data.MySqlClient;
 using Npgsql;
 
@@ -24,6 +26,40 @@ namespace JWLibrary.Database {
             where TDatabaseB : IDbConnection {
             if (typeof(TDatabaseA) == typeof(TDatabaseB)) throw new Exception("not allow same database connection.");
             return new Tuple<IDbConnection, IDbConnection>(Resolve<TDatabaseA>(), Resolve<TDatabaseB>());
+        }
+    }
+
+    public class JSqlEfDbContext: DbContext {
+        private readonly string _connectionString;
+
+        public JSqlEfDbContext() {
+        }
+
+        public JSqlEfDbContext(string connectionString) {
+            this._connectionString = connectionString;
+        }
+
+        public JSqlEfDbContext(DbContextOptions<JSqlEfDbContext> options) : base(options) {
+            
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(_connectionString);
+            //base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+    public class JMySqlEfDbContext : DbContext {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+    public class T2 {
+        public void Run() {
+            var context = new DbContextOptionsBuilder<JSqlEfDbContext>()
+                .UseSqlServer("").Options;
+
         }
     }
 }
