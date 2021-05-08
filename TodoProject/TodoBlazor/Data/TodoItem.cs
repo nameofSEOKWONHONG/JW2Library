9b8e30196902dd1d1181f58bd2a653dac826f347
SeokWon Hong <h20913@gmail.com>
2021-05-08 19:51:01 +0900

    add todo project

---
+++ TodoProject/TodoBlazor/Data/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoBlazor.TodoList {
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public bool IsToggle { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
