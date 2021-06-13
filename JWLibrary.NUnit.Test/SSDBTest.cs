e1440462f51b732e4eced2ba3e849c10bb6eb890
SeokWon Hong <h20913@gmail.com>
2021-06-13 16:08:38 +0900

    add ssdb client and test

---
+++ JWLibrary.NUnit.Test/SSDBTest.cs
@@ -0,0 +1,30 @@
+﻿using JWLibrary.Database;
+using NUnit.Framework;
+
+namespace JWLibrary.NUnit.Test {
+    public class SSDBTest {
+        private string ip = "192.168.137.245";
+        private int port = 8888;
+        
+        [Test]
+        public void ssdb_test1() {
+            using (var client = new SSDBClient(ip, port)) {
+                client.set("test", "test1");
+
+                var val = string.Empty;
+                client.get("test", out val);
+                Assert.AreEqual("test1", val);
+            }
+        }
+
+        [Test]
+        public void ssdb_del_test1() {
+            using (var client = new SSDBClient(ip, port)) {
+                client.del("test");
+                var val = string.Empty;
+                client.get("test", out val);
+                Assert.AreEqual(null, val);
+            }
+        }
+    }
+}
